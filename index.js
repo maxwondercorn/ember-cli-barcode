4132301b727f56ced99d01bbc75e71d991b564ab
maxwondercorn <gmartell@pobox.com>
2018-04-28 19:25:59 -0400

    non minimized jsbarcode in dev/test

--- index.js
+++ index.js
@@ -6,10 +6,15 @@ module.exports = {
 
   included(app) {
     this._super.included.apply(this, arguments);
-    app.import(app.bowerDirectory + '/JsBarcode/dist/JsBarcode.all.js');
+
+    app.import({
+      production: app.bowerDirectory + '/JsBarcode/dist/JsBarcode.all.min.js',
+      development: app.bowerDirectory + '/JsBarcode/dist/JsBarcode.all.js',
+      test: app.bowerDirectory + '/JsBarcode/dist/JsBarcode.all.js'
+    });
   },
 
   isDevelopingAddon() {
-    return true;
+    // return true;
   }
 };
