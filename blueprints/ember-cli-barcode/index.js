b7ebb9ea040ba14d54862124518ae9bd6e1a13ce
maxwondercorn <gmartell@pobox.com>
2018-07-18 23:58:22 -0400

    fixed test after removal of bower.json

--- blueprints/ember-cli-barcode/index.js
+++ blueprints/ember-cli-barcode/index.js
@@ -4,10 +4,4 @@ module.exports = {
 
   normalizeEntityName() {}, // no-op since we're just adding dependencies
 
-  afterInstall(/* options */) {
-    return this.addBowerPackagesToProject([
-      { name: 'JsBarcode', target: '3.8.0' }
-    ]);
-  }
-
 };
