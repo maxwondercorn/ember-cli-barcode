ad5bb30261c77d93b9413347d1ef2b44f95a172f
maxwondercorn <gregg.martell@gmail.com>
2018-08-08 20:08:18 -0400

    Feat/a11y alt and runtime configuration (#10)
    * added changelog
    
    * update readme
    
    * added alt-text and runtime configuration
    
    * added a11y attributes and runtime configuration
    
    * fixed eslint error causing test fails
    
    * changelog - added next release features
    
    * updated runtime configuration, added test for accessabity
    
    * updated readme - new features
    
    * clarified runtime config in readme
    
    * fixed tests for default alt text
    
    * refactered addon code, added aria-hidden and setTitle options

--- addon/components/bar-code.js
+++ addon/components/bar-code.js
@@ -1,57 +1,141 @@
-import Component from '@ember/component';
-import { get } from '@ember/object';
-import { computed } from '@ember/object';
-import { isBlank } from '@ember/utils';
+import Component from "@ember/component";
+import { get } from "@ember/object";
+import { computed } from "@ember/object";
+import { isBlank } from "@ember/utils";
+import { getOwner } from "@ember/application";
 
 /* global JsBarcode */
 
 export default Component.extend({
-  tagName: 'svg',
+  tagName: "svg",
   thisId: null,
+  defaultText: "Barcode value",
+  altText: null,
+  excludeAltValue: false,
+  svgns: "http://www.w3.org/2000/svg",
+
+  // get config from enviroment.js
+  // https://stackoverflow.com/questions/42002664/accessing-ember-environment-config-env-from-addon
+  cfg: computed(function() {
+    return (
+      getOwner(this).resolveRegistration("config:environment")[
+        "ember-cli-barcode"
+      ] || {}
+    );
+  }),
 
   // jsbarcode options
   // https://github.com/lindell/JsBarcode/wiki/Options#format
 
-  defaults: computed(function () {
+  defaults: computed(function() {
+    let cfg = this.get("cfg");
+
     return {
-      format:       this.get('format') || "CODE128",
-      mod43:        this.get('mod43') || false,   // only used with code39 barcodes
-      width:        this.get('width') || 2,
-      height:       this.get('height') || 100,
-      displayValue: isBlank(this.get('displayValue')) ? true : this.get('displayValue'),
-      fontOptions:  this.get('fontOptions') || "",
-      font:         this.get('font') || "monospace",
-      textAlign:    this.get('textAlign') || "center",
-      textPosition: this.get('textPosition') || "bottom",
-      textMargin:   this.get('textMargin') || 2,
-      fontSize:     this.get('fontSize') || 20,
-      background:   this.get('background') || "#ffffff",
-      lineColor:    this.get('lineColor') || "#000000",
-      margin:       isBlank(this.get('margin')) ? 10 : this.get('margin'),
-      marginTop:    isBlank(this.get('marginTop')) ? undefined : this.get('marginTop'),
-      marginBottom: isBlank(this.get('marginBottom')) ? undefined : this.get('marginBottom'),
-      marginLeft:   isBlank(this.get('marginLeft')) ? undefined : this.get('marginLeft'),
-      marginRight:  isBlank(this.get('marginRight')) ? undefined : this.get('marginRight'),
-      flat:         this.get('flat') || false,
-      lastChar:     this.get('lastChar') || ''
-    }
+      format: this.get("format") || cfg.format || "CODE128",
+      mod43: this.get("mod43") || cfg.mod43 || false, // only used with code39 barcodes
+      width: this.get("width") || cfg.width || 2,
+      height: this.get("height") || cfg.height || 100,
+
+      displayValue: isBlank(this.get("displayValue"))
+        ? isBlank(cfg.displayValue)
+          ? true
+          : cfg.displayValue
+        : true,
+
+      fontOptions: this.get("fontOptions") || cfg.fontOptions || "",
+      font: this.get("font") || cfg.font || "monospace",
+      textAlign: this.get("textAlign") || cfg.textAlign || "center",
+      textPosition: this.get("textPosition") || cfg.textPosition || "bottom",
+      textMargin: this.get("textMargin") || cfg.textMargin || 2,
+      fontSize: this.get("fontSize") || cfg.fontSize || 20,
+      background: this.get("background") || cfg.background || "#ffffff",
+      lineColor: this.get("lineColor") || cfg.lineColor || "#000000",
+      margin: isBlank(this.get("margin")) ? 10 : this.get("margin"),
+
+      marginTop: isBlank(this.get("marginTop"))
+        ? cfg.marginTop || undefined
+        : this.get("marginTop"),
+
+      marginBottom: isBlank(this.get("marginBottom"))
+        ? cfg.marginBottom || undefined
+        : this.get("marginBottom"),
+
+      marginLeft: isBlank(this.get("marginLeft"))
+        ? cfg.marginLeft || undefined
+        : this.get("marginLeft"),
+
+      marginRight: isBlank(this.get("marginRight"))
+        ? cfg.marginRight || undefined
+        : this.get("marginRight"),
+
+      flat: this.get("flat") || cfg.flat || false,
+      lastChar: this.get("lastChar") || cfg.lastChar || ""
+    };
   }),
 
-  didInsertElement () {
-    this.set('thisId', this.elementId);
+  didInsertElement() {
+    this.set("thisId", this.elementId);
   },
 
   didRender() {
     this._super(...arguments);
+    let title;
 
     // if options object is passed in, use it
-    let options = this.get('options') || this.get('defaults');
-  
+    let options = this.get("options") || this.get("defaults");
+
     // set the call back on options
-    options['valid'] = (status) => this.valid && this.valid(status);
+    options["valid"] = status => this.valid && this.valid(status);
 
     // now render the barcode
-    JsBarcode(`#${this.get('thisId')}`, get(this, 'value'), options);
-  },
+    JsBarcode(`#${this.get("thisId")}`, get(this, "value"), options);
 
+    // add accessability to barcode
+    // do after render because svg is cleared by jsbarcode
+    // https://medium.com/statuscode/getting-started-with-website-accessibility-5586c7febc92
+
+    let cfg = this.get("cfg");
+
+    // if they want to hide them, apply and exit
+    if (cfg.ariaHidden) {
+      this.element.setAttribute("aria-hidden", "true");
+      return;
+    }
+
+    let text = `${this.get("altText") ||
+      cfg.altText ||
+      this.get("defaultText")}`;
+
+    // exclude the value from the alt text?
+    if (!cfg.excludeAltValue) text = `${text} ${this.get("value")}`;
+
+    switch (this.element.nodeName) {
+      // add alt text attribute
+      // http://a11y-style-guide.com/style-guide/section-media.html#kssref-media-images
+      case "IMG":
+        this.element.setAttribute("alt", text);
+        break;
+
+      // add title and aria-labelledby attritubte
+      // http://a11y-style-guide.com/style-guide/section-media.html#kssref-media-svgs
+      case "svg":
+        this.element.setAttribute("aria-labelledby", "title");
+        title = document.createElementNS(this.get("svgns"), "title");
+        title.innerHTML = text;
+        this.element.appendChild(title);
+        break;
+
+      // what do you do to canvas?
+      // https://www.w3.org/Talks/2014/0510-canvas-a11y/#1
+      // http://pauljadam.com/demos/canvas.html
+      case "CANVAS":
+        this.element.setAttribute("role", "img");
+        this.element.setAttribute("aria-label", text);
+        break;
+    }
+    if (cfg.setTitle) {
+      // redundant on svg, but it doens't hurt
+      this.element.setAttribute("title", text);
+    }
+  }
 });
--- tests/integration/components/bar-code-test.js
+++ tests/integration/components/bar-code-test.js
@@ -1,120 +1,144 @@
-import { moduleForComponent, test } from 'ember-qunit';
-import hbs from 'htmlbars-inline-precompile';
+import { moduleForComponent, test } from "ember-qunit";
+import hbs from "htmlbars-inline-precompile";
 /* debug: true */
 
-moduleForComponent('bar-code', 'Integration | Component | bar code', {
+moduleForComponent("bar-code", "Integration | Component | bar code", {
   integration: true
 });
 
-test('it renders, but not displaye', function (assert) {
+test("it renders, but not displaye", function(assert) {
   assert.expect(1);
 
   // use if you need to pause during the test
   // const done = assert.async();
   // setTimeout(() => done(), 20000);
 
-  this.set('code', "1234567");
+  this.set("code", "1234567");
 
   // will not dispaly - text value not set
   this.render(hbs`{{bar-code}}`);
-  assert.equal(this.$('svg').attr('style'), undefined);
-
+  assert.equal(this.$("svg").attr("style"), undefined);
 });
 
-test('it renders a SVG barcode', function (assert) {
-  assert.expect(1);
- 
-  this.set('code', "1234567");
+test("it renders a SVG barcode", function(assert) {
+  assert.expect(3);
+
+  this.set("code", "1234567");
 
   // will display - text value is set
   this.render(hbs`{{bar-code value=code}}`);
-  assert.equal(this.$('svg').attr('style'), 'transform: translate(0,0)');
+  assert.equal(this.$("svg").attr("style"), "transform: translate(0,0)");
 
+  // verify the aria-labelledby attribute is set
+  assert.equal(
+    this.$("svg").attr("aria-labelledby"),
+    "title",
+    "SVG title attribute is created"
+  );
+
+  // verify title element is set to correct text
+  assert.equal(
+    this.$("svg").find("title")[0].innerHTML,
+    "Barcode value 1234567",
+    "SVG title element has correct innerHTML"
+  );
 });
 
-test('it renders a IMG barcode', function (assert) {
-  assert.expect(1);
+test("it renders a IMG barcode", function(assert) {
+  assert.expect(2);
 
-  this.set('code', "1234567");
+  this.set("code", "1234567");
 
   // render barcode as a image
   this.render(hbs`{{bar-code tagName="img" value=code}}`);
-  assert.equal((this.$('img').prop('src')).substr(0, 10), 'data:image');
+  assert.equal(
+    this.$("img")
+      .prop("src")
+      .substr(0, 10),
+    "data:image"
+  );
 
+  // verify the alt attribute is set
+  assert.equal(this.$("img").attr("alt"), "Barcode value 1234567");
 });
 
-test('it renders a CANVAS barcode', function (assert) {
-  assert.expect(1);
+test("it renders a CANVAS barcode", function(assert) {
+  assert.expect(3);
 
-  this.set('code', "1234567");
+  this.set("code", "1234567");
 
   // render barcode as a canvas
   this.render(hbs`{{bar-code tagName="canvas" value=code}}`);
-  assert.equal(this.$('canvas').length, 1)
+  assert.equal(this.$("canvas").length, 1);
 
+  // verify canvas attributes are set
+  assert.equal(this.$("canvas").attr("aria-label"), "Barcode value 1234567");
+  assert.equal(this.$("canvas").attr("role"), "img");
 });
 
-test('it renders barcode with red backgound', function (assert) {
+test("it renders barcode with red backgound", function(assert) {
   assert.expect(1);
 
-  this.set('code', "1234567");
+  this.set("code", "1234567");
 
   // change barcode background to red
   this.render(hbs`{{bar-code value=code background="red"}}`);
-  assert.equal(this.$('rect').attr('style'), 'fill:red;');
-
+  assert.equal(this.$("rect").attr("style"), "fill:red;");
 });
 
-test('it renders a blue colored barcode', function (assert) {
+test("it renders a blue colored barcode", function(assert) {
   assert.expect(1);
 
-  this.set('code', "1234567");
+  this.set("code", "1234567");
 
   // change barcode color to blue
   this.render(hbs`{{bar-code value=code lineColor="blue"}}`);
-  assert.equal(this.$('g').attr('style'), 'fill:blue;');
-
+  assert.equal(this.$("g").attr("style"), "fill:blue;");
 });
 
-test('it renders with options, not options', function (assert) {
+test("it renders with options, not options", function(assert) {
   assert.expect(1);
 
-  this.set('code', "1234567");
+  // `getOwner(this).resolveRegistration('config:environment')` 
+  // then in your test
+  // 's `beforeEach()` you can do 
+  // `this.owner.resolveRegistration('config:environment').foo = 'bar'`
 
-  this.set('options', {
+  this.set("code", "1234567");
+
+  this.set("options", {
     format: "CODE39",
     textPosition: "top",
     lineColor: "#eeeeeee"
-  })
+  });
 
   // render using options which overrides other options
   this.render(hbs`{{bar-code value=code options=options lineColor="green"}}`);
-  assert.equal(this.$('g').attr('style'), 'fill:#eeeeeee;');
-
+  assert.equal(this.$("g").attr("style"), "fill:#eeeeeee;");
 });
 
-test('it sends the valid status to the action', function (assert) {
+test("it sends the valid status to the action", function(assert) {
   assert.expect(1);
 
-  this.set('checkValid', (status) => {
+  this.set("checkValid", status => {
     assert.equal(status, true);
   });
 
-  this.set('code', "1234567");
+  this.set("code", "1234567");
 
   this.render(hbs`{{bar-code value=code valid=(action checkValid)}}`);
-
 });
 
-test('it sends false for invalid invalid code', function (assert) {
+test("it sends false for invalid invalid code", function(assert) {
   assert.expect(1);
 
-  this.set('checkValid', (status) => {
+  this.set("checkValid", status => {
     assert.equal(status, false);
   });
 
-  this.set('code', "1234");
+  this.set("code", "1234");
 
-  this.render(hbs`{{bar-code value=code format="EAN8" valid=(action checkValid)}}`);
-
+  this.render(
+    hbs`{{bar-code value=code format="EAN8" valid=(action checkValid)}}`
+  );
 });
