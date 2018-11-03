563b6460a322195ada1967c7f94bc47621af267b
maxwondercorn <gmartell@pobox.com>
2018-11-03 13:46:54 -0400

    refactored this.get

--- addon/components/bar-code.js
+++ addon/components/bar-code.js
@@ -1,5 +1,4 @@
 import Component from "@ember/component";
-import { get } from "@ember/object";
 import { computed } from "@ember/object";
 import { isBlank } from "@ember/utils";
 import { getOwner } from "@ember/application";
@@ -16,7 +15,7 @@ export default Component.extend({
 
   // get config from enviroment.js
   // https://stackoverflow.com/questions/42002664/accessing-ember-environment-config-env-from-addon
-  cfg: computed(function() {
+  config: computed(function() {
     return (
       getOwner(this).resolveRegistration("config:environment")[
         "ember-cli-barcode"
@@ -28,48 +27,48 @@ export default Component.extend({
   // https://github.com/lindell/JsBarcode/wiki/Options#format
 
   defaults: computed(function() {
-    let cfg = this.get("cfg");
+    let cfg = this.config;
 
     return {
-      format: this.get("format") || cfg.format || "CODE128",
-      mod43: this.get("mod43") || cfg.mod43 || false, // only used with code39 barcodes
-      width: this.get("width") || cfg.width || 2,
-      height: this.get("height") || cfg.height || 100,
+      format: this.format || cfg.format || "CODE128",
+      mod43: this.mod43 || cfg.mod43 || false, // only used with code39 barcodes
+      width: this.width || cfg.width || 2,
+      height: this.height || cfg.height || 100,
 
-      displayValue: isBlank(this.get("displayValue"))
+      displayValue: isBlank(this.displayValue)
         ? isBlank(cfg.displayValue)
           ? true
           : cfg.displayValue
         : true,
 
-      fontOptions: this.get("fontOptions") || cfg.fontOptions || "",
-      font: this.get("font") || cfg.font || "monospace",
-      textAlign: this.get("textAlign") || cfg.textAlign || "center",
-      textPosition: this.get("textPosition") || cfg.textPosition || "bottom",
-      textMargin: this.get("textMargin") || cfg.textMargin || 2,
-      fontSize: this.get("fontSize") || cfg.fontSize || 20,
-      background: this.get("background") || cfg.background || "#ffffff",
-      lineColor: this.get("lineColor") || cfg.lineColor || "#000000",
-      margin: isBlank(this.get("margin")) ? 10 : this.get("margin"),
+      fontOptions: this.fontOptions || cfg.fontOptions || "",
+      font: this.font || cfg.font || "monospace",
+      textAlign: this.textAlign || cfg.textAlign || "center",
+      textPosition: this.textPosition || cfg.textPosition || "bottom",
+      textMargin: this.textMargin || cfg.textMargin || 2,
+      fontSize: this.fontSize || cfg.fontSize || 20,
+      background: this.background || cfg.background || "#ffffff",
+      lineColor: this.lineColor || cfg.lineColor || "#000000",
+      margin: isBlank(this.margin) ? 10 : this.margin,
 
-      marginTop: isBlank(this.get("marginTop"))
+      marginTop: isBlank(this.marginTop)
         ? cfg.marginTop || undefined
-        : this.get("marginTop"),
+        : this.marginTop,
 
-      marginBottom: isBlank(this.get("marginBottom"))
+      marginBottom: isBlank(this.marginBottom)
         ? cfg.marginBottom || undefined
-        : this.get("marginBottom"),
+        : this.marginBottom,
 
-      marginLeft: isBlank(this.get("marginLeft"))
+      marginLeft: isBlank(this.marginLeft)
         ? cfg.marginLeft || undefined
-        : this.get("marginLeft"),
+        : this.marginLeft,
 
-      marginRight: isBlank(this.get("marginRight"))
+      marginRight: isBlank(this.marginRight)
         ? cfg.marginRight || undefined
-        : this.get("marginRight"),
+        : this.marginRight,
 
-      flat: this.get("flat") || cfg.flat || false,
-      lastChar: this.get("lastChar") || cfg.lastChar || ""
+      flat: this.flat || cfg.flat || false,
+      lastChar: this.lastChar || cfg.lastChar || ""
     };
   }),
 
@@ -82,19 +81,19 @@ export default Component.extend({
     let title;
 
     // if options object is passed in, use it
-    let options = this.get("options") || this.get("defaults");
+    let options = this.options || this.defaults;
 
     // set the call back on options
     options["valid"] = status => this.valid && this.valid(status);
 
     // now render the barcode
-    JsBarcode(`#${this.get("thisId")}`, get(this, "value"), options);
+    JsBarcode(`#${this.thisId}`, this.value, options);
 
     // add accessability to barcode
     // do after render because svg is cleared by jsbarcode
     // https://medium.com/statuscode/getting-started-with-website-accessibility-5586c7febc92
 
-    let cfg = this.get("cfg");
+    let cfg = this.config;
 
     // if they want to hide them, apply and exit
     if (cfg.ariaHidden) {
@@ -102,12 +101,12 @@ export default Component.extend({
       return;
     }
 
-    let text = `${this.get("altText") ||
+    let text = `${this.altText ||
       cfg.altText ||
-      this.get("defaultText")}`;
+      this.defaultText}`;
 
     // exclude the value from the alt text?
-    if (!cfg.excludeAltValue) text = `${text} ${this.get("value")}`;
+    if (!cfg.excludeAltValue) text = `${text} ${this.value}`;
 
     switch (this.element.nodeName) {
       // add alt text attribute
@@ -120,7 +119,7 @@ export default Component.extend({
       // http://a11y-style-guide.com/style-guide/section-media.html#kssref-media-svgs
       case "svg":
         this.element.setAttribute("aria-labelledby", "title");
-        title = document.createElementNS(this.get("svgns"), "title");
+        title = document.createElementNS(this.svgns, "title");
         title.innerHTML = text;
         this.element.appendChild(title);
         break;
