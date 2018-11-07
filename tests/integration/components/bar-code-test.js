61beaed2afcc32c0aa38b1c4dea1c89fbd95854b
maxwondercorn <gmartell@pobox.com>
2018-11-07 00:13:55 -0500

    ugraded qunit tests

--- tests/integration/components/bar-code-test.js
+++ tests/integration/components/bar-code-test.js
@@ -1,144 +1,142 @@
-import { moduleForComponent, test } from "ember-qunit";
+import { find, findAll, render } from '@ember/test-helpers';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from "ember-qunit";
 import hbs from "htmlbars-inline-precompile";
-/* debug: true */
 
-moduleForComponent("bar-code", "Integration | Component | bar code", {
-  integration: true
-});
+module("Integration | Component | bar code", function(hooks) {
+  setupRenderingTest(hooks);
 
-test("it renders, but not displaye", function(assert) {
-  assert.expect(1);
+  test("it renders, but not displaye", async function(assert) {
+    assert.expect(1);
 
-  // use if you need to pause during the test
-  // const done = assert.async();
-  // setTimeout(() => done(), 20000);
+    // use if you need to pause during the test
+    // const done = assert.async();
+    // setTimeout(() => done(), 20000);
 
-  this.set("code", "1234567");
+    this.set("code", "1234567");
 
-  // will not dispaly - text value not set
-  this.render(hbs`{{bar-code}}`);
-  assert.equal(this.$("svg").attr("style"), undefined);
-});
-
-test("it renders a SVG barcode", function(assert) {
-  assert.expect(3);
-
-  this.set("code", "1234567");
-
-  // will display - text value is set
-  this.render(hbs`{{bar-code value=code}}`);
-  assert.equal(this.$("svg").attr("style"), "transform: translate(0,0)");
-
-  // verify the aria-labelledby attribute is set
-  assert.equal(
-    this.$("svg").attr("aria-labelledby"),
-    "title",
-    "SVG title attribute is created"
-  );
-
-  // verify title element is set to correct text
-  assert.equal(
-    this.$("svg").find("title")[0].innerHTML,
-    "Barcode value 1234567",
-    "SVG title element has correct innerHTML"
-  );
-});
-
-test("it renders a IMG barcode", function(assert) {
-  assert.expect(2);
-
-  this.set("code", "1234567");
-
-  // render barcode as a image
-  this.render(hbs`{{bar-code tagName="img" value=code}}`);
-  assert.equal(
-    this.$("img")
-      .prop("src")
-      .substr(0, 10),
-    "data:image"
-  );
-
-  // verify the alt attribute is set
-  assert.equal(this.$("img").attr("alt"), "Barcode value 1234567");
-});
-
-test("it renders a CANVAS barcode", function(assert) {
-  assert.expect(3);
-
-  this.set("code", "1234567");
-
-  // render barcode as a canvas
-  this.render(hbs`{{bar-code tagName="canvas" value=code}}`);
-  assert.equal(this.$("canvas").length, 1);
-
-  // verify canvas attributes are set
-  assert.equal(this.$("canvas").attr("aria-label"), "Barcode value 1234567");
-  assert.equal(this.$("canvas").attr("role"), "img");
-});
-
-test("it renders barcode with red backgound", function(assert) {
-  assert.expect(1);
-
-  this.set("code", "1234567");
-
-  // change barcode background to red
-  this.render(hbs`{{bar-code value=code background="red"}}`);
-  assert.equal(this.$("rect").attr("style"), "fill:red;");
-});
-
-test("it renders a blue colored barcode", function(assert) {
-  assert.expect(1);
-
-  this.set("code", "1234567");
-
-  // change barcode color to blue
-  this.render(hbs`{{bar-code value=code lineColor="blue"}}`);
-  assert.equal(this.$("g").attr("style"), "fill:blue;");
-});
-
-test("it renders with options, not options", function(assert) {
-  assert.expect(1);
-
-  // `getOwner(this).resolveRegistration('config:environment')` 
-  // then in your test
-  // 's `beforeEach()` you can do 
-  // `this.owner.resolveRegistration('config:environment').foo = 'bar'`
-
-  this.set("code", "1234567");
-
-  this.set("options", {
-    format: "CODE39",
-    textPosition: "top",
-    lineColor: "#eeeeeee"
+    // will not dispaly - text value not set
+    await render(hbs`{{bar-code}}`);
+    assert.equal(find("svg").getAttribute("style"), undefined);
   });
 
-  // render using options which overrides other options
-  this.render(hbs`{{bar-code value=code options=options lineColor="green"}}`);
-  assert.equal(this.$("g").attr("style"), "fill:#eeeeeee;");
-});
+  test("it renders a SVG barcode", async function(assert) {
+    assert.expect(3);
 
-test("it sends the valid status to the action", function(assert) {
-  assert.expect(1);
+    this.set("code", "1234567");
 
-  this.set("checkValid", status => {
-    assert.equal(status, true);
+    // will display - text value is set
+    await render(hbs`{{bar-code value=code}}`);
+    assert.equal(find("svg").getAttribute("style"), "transform: translate(0,0)");
+
+    // verify the aria-labelledby attribute is set
+    assert.equal(
+      find("svg").getAttribute("aria-labelledby"),
+      "title",
+      "SVG title attribute is created"
+    );
+
+    // verify title element is set to correct text
+    assert.equal(
+      this.$("svg").find("title")[0].innerHTML,
+      "Barcode value 1234567",
+      "SVG title element has correct innerHTML"
+    );
   });
 
-  this.set("code", "1234567");
+  test("it renders a IMG barcode", async function(assert) {
+    assert.expect(2);
 
-  this.render(hbs`{{bar-code value=code valid=(action checkValid)}}`);
-});
+    this.set("code", "1234567");
 
-test("it sends false for invalid invalid code", function(assert) {
-  assert.expect(1);
+    // render barcode as a image
+    await render(hbs`{{bar-code tagName="img" value=code}}`);
+    assert.equal(
+      find("img").src
+        .substr(0, 10),
+      "data:image"
+    );
 
-  this.set("checkValid", status => {
-    assert.equal(status, false);
+    // verify the alt attribute is set
+    assert.equal(find("img").getAttribute("alt"), "Barcode value 1234567");
   });
 
-  this.set("code", "1234");
+  test("it renders a CANVAS barcode", async function(assert) {
+    assert.expect(3);
 
-  this.render(
-    hbs`{{bar-code value=code format="EAN8" valid=(action checkValid)}}`
-  );
+    this.set("code", "1234567");
+
+    // render barcode as a canvas
+    await render(hbs`{{bar-code tagName="canvas" value=code}}`);
+    assert.equal(findAll("canvas").length, 1);
+
+    // verify canvas attributes are set
+    assert.equal(find("canvas").getAttribute("aria-label"), "Barcode value 1234567");
+    assert.equal(find("canvas").getAttribute("role"), "img");
+  });
+
+  test("it renders barcode with red backgound", async function(assert) {
+    assert.expect(1);
+
+    this.set("code", "1234567");
+
+    // change barcode background to red
+    await render(hbs`{{bar-code value=code background="red"}}`);
+    assert.equal(find("rect").getAttribute("style"), "fill:red;");
+  });
+
+  test("it renders a blue colored barcode", async function(assert) {
+    assert.expect(1);
+
+    this.set("code", "1234567");
+
+    // change barcode color to blue
+    await render(hbs`{{bar-code value=code lineColor="blue"}}`);
+    assert.equal(find("g").getAttribute("style"), "fill:blue;");
+  });
+
+  test("it renders with options, not options", async function(assert) {
+    assert.expect(1);
+
+    // `getOwner(this).resolveRegistration('config:environment')` 
+    // then in your test
+    // 's `beforeEach()` you can do 
+    // `this.owner.resolveRegistration('config:environment').foo = 'bar'`
+
+    this.set("code", "1234567");
+
+    this.set("options", {
+      format: "CODE39",
+      textPosition: "top",
+      lineColor: "#eeeeeee"
+    });
+
+    // render using options which overrides other options
+    await render(hbs`{{bar-code value=code options=options lineColor="green"}}`);
+    assert.equal(find("g").getAttribute("style"), "fill:#eeeeeee;");
+  });
+
+  test("it sends the valid status to the action", async function(assert) {
+    assert.expect(1);
+
+    this.set("checkValid", status => {
+      assert.equal(status, true);
+    });
+
+    this.set("code", "1234567");
+
+    await render(hbs`{{bar-code value=code valid=(action checkValid)}}`);
+  });
+
+  test("it sends false for invalid invalid code", async function(assert) {
+    assert.expect(1);
+
+    this.set("checkValid", status => {
+      assert.equal(status, false);
+    });
+
+    this.set("code", "1234");
+
+    await render(hbs`{{bar-code value=code format="EAN8" valid=(action checkValid)}}`);
+  });
 });
