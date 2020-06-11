7a832271136e2d0409ae23948447214fe253ab8d
maxwondercorn <gmartell@pobox.com>
2020-06-11 12:21:22 -0400

    Corrections based on updated ember linting rules

--- addon/components/bar-code.js
+++ addon/components/bar-code.js
@@ -1,24 +1,24 @@
-import Component from "@ember/component";
-import { computed } from "@ember/object";
-import { isBlank } from "@ember/utils";
-import { getOwner } from "@ember/application";
+import Component from '@ember/component';
+import { computed } from '@ember/object';
+import { isBlank } from '@ember/utils';
+import { getOwner } from '@ember/application';
 
 /* global JsBarcode */
 
 export default Component.extend({
-  tagName: "svg",
+  tagName: 'svg',
   thisId: null,
-  defaultText: "Barcode value",
+  defaultText: 'Barcode value',
   altText: null,
   excludeAltValue: false,
-  svgns: "http://www.w3.org/2000/svg",
+  svgns: 'http://www.w3.org/2000/svg',
 
   // get config from enviroment.js
   // https://stackoverflow.com/questions/42002664/accessing-ember-environment-config-env-from-addon
-  config: computed(function() {
+  config: computed(function () {
     return (
-      getOwner(this).resolveRegistration("config:environment")[
-        "ember-cli-barcode"
+      getOwner(this).resolveRegistration('config:environment')[
+        'ember-cli-barcode'
       ] || {}
     );
   }),
@@ -26,54 +26,78 @@ export default Component.extend({
   // jsbarcode options
   // https://github.com/lindell/JsBarcode/wiki/Options#format
 
-  defaults: computed(function() {
-    let cfg = this.config;
+  defaults: computed(
+    'background',
+    'config',
+    'displayValue',
+    'flat',
+    'font',
+    'fontOptions',
+    'fontSize',
+    'format',
+    'height',
+    'lastChar',
+    'lineColor',
+    'margin',
+    'marginBottom',
+    'marginLeft',
+    'marginRight',
+    'marginTop',
+    'mod43',
+    'textAlign',
+    'textMargin',
+    'textPosition',
+    'width',
+    
+    function () {
+      let cfg = this.config;
 
-    return {
-      format: this.format || cfg.format || "CODE128",
-      mod43: this.mod43 || cfg.mod43 || false, // only used with code39 barcodes
-      width: this.width || cfg.width || 2,
-      height: this.height || cfg.height || 100,
+      return {
+        format: this.format || cfg.format || 'CODE128',
+        mod43: this.mod43 || cfg.mod43 || false, // only used with code39 barcodes
+        width: this.width || cfg.width || 2,
+        height: this.height || cfg.height || 100,
 
-      displayValue: isBlank(this.displayValue)
-        ? isBlank(cfg.displayValue)
-          ? true
-          : cfg.displayValue
-        : true,
+        displayValue: isBlank(this.displayValue)
+          ? isBlank(cfg.displayValue)
+            ? true
+            : cfg.displayValue
+          : true,
 
-      fontOptions: this.fontOptions || cfg.fontOptions || "",
-      font: this.font || cfg.font || "monospace",
-      textAlign: this.textAlign || cfg.textAlign || "center",
-      textPosition: this.textPosition || cfg.textPosition || "bottom",
-      textMargin: this.textMargin || cfg.textMargin || 2,
-      fontSize: this.fontSize || cfg.fontSize || 20,
-      background: this.background || cfg.background || "#ffffff",
-      lineColor: this.lineColor || cfg.lineColor || "#000000",
-      margin: isBlank(this.margin) ? 10 : this.margin,
+        fontOptions: this.fontOptions || cfg.fontOptions || '',
+        font: this.font || cfg.font || 'monospace',
+        textAlign: this.textAlign || cfg.textAlign || 'center',
+        textPosition: this.textPosition || cfg.textPosition || 'bottom',
+        textMargin: this.textMargin || cfg.textMargin || 2,
+        fontSize: this.fontSize || cfg.fontSize || 20,
+        background: this.background || cfg.background || '#ffffff',
+        lineColor: this.lineColor || cfg.lineColor || '#000000',
+        margin: isBlank(this.margin) ? 10 : this.margin,
 
-      marginTop: isBlank(this.marginTop)
-        ? cfg.marginTop || undefined
-        : this.marginTop,
+        marginTop: isBlank(this.marginTop)
+          ? cfg.marginTop || undefined
+          : this.marginTop,
 
-      marginBottom: isBlank(this.marginBottom)
-        ? cfg.marginBottom || undefined
-        : this.marginBottom,
+        marginBottom: isBlank(this.marginBottom)
+          ? cfg.marginBottom || undefined
+          : this.marginBottom,
 
-      marginLeft: isBlank(this.marginLeft)
-        ? cfg.marginLeft || undefined
-        : this.marginLeft,
+        marginLeft: isBlank(this.marginLeft)
+          ? cfg.marginLeft || undefined
+          : this.marginLeft,
 
-      marginRight: isBlank(this.marginRight)
-        ? cfg.marginRight || undefined
-        : this.marginRight,
+        marginRight: isBlank(this.marginRight)
+          ? cfg.marginRight || undefined
+          : this.marginRight,
 
-      flat: this.flat || cfg.flat || false,
-      lastChar: this.lastChar || cfg.lastChar || ""
-    };
-  }),
+        flat: this.flat || cfg.flat || false,
+        lastChar: this.lastChar || cfg.lastChar || ''
+      };
+    }
+  ),
 
   didInsertElement() {
-    this.set("thisId", this.elementId);
+    this.set('thisId', this.elementId);
   },
 
   didRender() {
@@ -84,7 +108,7 @@ export default Component.extend({
     let options = this.options || this.defaults;
 
     // set the call back on options
-    options["valid"] = status => this.valid && this.valid(status);
+    options['valid'] = (status) => this.valid && this.valid(status);
 
     // now render the barcode
     JsBarcode(`#${this.thisId}`, this.value, options);
@@ -97,13 +121,11 @@ export default Component.extend({
 
     // if they want to hide them, apply and exit
     if (cfg.ariaHidden) {
-      this.element.setAttribute("aria-hidden", "true");
+      this.element.setAttribute('aria-hidden', 'true');
       return;
     }
 
-    let text = `${this.altText ||
-      cfg.altText ||
-      this.defaultText}`;
+    let text = `${this.altText || cfg.altText || this.defaultText}`;
 
     // exclude the value from the alt text?
     if (!cfg.excludeAltValue) text = `${text} ${this.value}`;
@@ -111,15 +133,15 @@ export default Component.extend({
     switch (this.element.nodeName) {
       // add alt text attribute
       // http://a11y-style-guide.com/style-guide/section-media.html#kssref-media-images
-      case "IMG":
-        this.element.setAttribute("alt", text);
+      case 'IMG':
+        this.element.setAttribute('alt', text);
         break;
 
       // add title and aria-labelledby attritubte
       // http://a11y-style-guide.com/style-guide/section-media.html#kssref-media-svgs
-      case "svg":
-        this.element.setAttribute("aria-labelledby", "title");
-        title = document.createElementNS(this.svgns, "title");
+      case 'svg':
+        this.element.setAttribute('aria-labelledby', 'title');
+        title = document.createElementNS(this.svgns, 'title');
         title.innerHTML = text;
         this.element.appendChild(title);
         break;
@@ -127,14 +149,14 @@ export default Component.extend({
       // what do you do to canvas?
       // https://www.w3.org/Talks/2014/0510-canvas-a11y/#1
       // http://pauljadam.com/demos/canvas.html
-      case "CANVAS":
-        this.element.setAttribute("role", "img");
-        this.element.setAttribute("aria-label", text);
+      case 'CANVAS':
+        this.element.setAttribute('role', 'img');
+        this.element.setAttribute('aria-label', text);
         break;
     }
     if (cfg.setTitle) {
       // redundant on svg, but it doens't hurt
-      this.element.setAttribute("title", text);
+      this.element.setAttribute('title', text);
     }
   }
 });
--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -1,133 +1,134 @@
-import { action, computed } from "@ember/object";
-import Controller from "@ember/controller";
-import config from "../config/environment";
+import { action, computed } from '@ember/object';
+import Controller from '@ember/controller';
+import config from '../config/environment';
 
 export default class ApplicationController extends Controller {
   appVersion = config.APP.appVersion;
 
-  format = "CODE128";
+  format = 'CODE128';
   width = 2;
   height = 100;
   displayValue = true;
   fontSize = 20;
   fontOption = null;
-  font = "monospace";
-  textAlign = "center";
-  textPosition = "bottom";
-  background = "#ffffff";
-  lineColor = "#000000";
+  font = 'monospace';
+  textAlign = 'center';
+  textPosition = 'bottom';
+  background = '#ffffff';
+  lineColor = '#000000';
   validCode = false;
-  testValue = "96385074";
+  testValue = '96385074';
 
   formats = [
-    { value: "CODE128", code: "123456", selected: true },
-    { value: "EAN13", code: "5901234123457" },
-    { value: "UPC", code: "123456789999" },
-    { value: "EAN8", code: "96385074" },
-    { value: "EAN5", code: "54495" },
-    { value: "EAN2", code: "53" },
-    { value: "CODE39", code: "CODE39" },
-    { value: "ITF14", code: "12345678901231" },
-    { value: "MSI", code: "1234" },
-    { value: "MSI10", code: "1234" },
-    { value: "MSI11", code: "1234" },
-    { value: "MSI1010", code: "1234" },
-    { value: "MSI1110", code: "1234" },
-    { value: "pharmacode", code: "1234" },
-    { value: "codabar", code: "1234567890" }
+    { value: 'CODE128', code: '123456', selected: true },
+    { value: 'EAN13', code: '5901234123457' },
+    { value: 'UPC', code: '123456789999' },
+    { value: 'EAN8', code: '96385074' },
+    { value: 'EAN5', code: '54495' },
+    { value: 'EAN2', code: '53' },
+    { value: 'CODE39', code: 'CODE39' },
+    { value: 'ITF14', code: '12345678901231' },
+    { value: 'MSI', code: '1234' },
+    { value: 'MSI10', code: '1234' },
+    { value: 'MSI11', code: '1234' },
+    { value: 'MSI1010', code: '1234' },
+    { value: 'MSI1110', code: '1234' },
+    { value: 'pharmacode', code: '1234' },
+    { value: 'codabar', code: '1234567890' }
   ];
 
-  widths = [{ value: "1" }, { value: "2", selected: true }, { value: "3" }];
+  widths = [{ value: '1' }, { value: '2', selected: true }, { value: '3' }];
 
   heights = [
-    { value: "50" },
-    { value: "100", selected: true },
-    { value: "200" }
+    { value: '50' },
+    { value: '100', selected: true },
+    { value: '200' }
   ];
 
   dispalyValues = [{ value: true, selected: true }, { value: false }];
 
   fontSizes = [
-    { value: "5" },
-    { value: "10" },
-    { value: "15" },
-    { value: "20", selected: true },
-    { value: "25" },
-    { value: "30" }
+    { value: '5' },
+    { value: '10' },
+    { value: '15' },
+    { value: '20', selected: true },
+    { value: '25' },
+    { value: '30' }
   ];
 
   fontOptions = [
-    { value: "", selected: true },
-    { value: "italic" },
-    { value: "bold" },
-    { value: "oblique" },
-    { value: "bold, italic" }
+    { value: '', selected: true },
+    { value: 'italic' },
+    { value: 'bold' },
+    { value: 'oblique' },
+    { value: 'bold, italic' }
   ];
 
   fonts = [
-    { value: "monospace", selected: true },
-    { value: "Arial" },
-    { value: "Arial Narrow" },
-    { value: "Helvetica" },
-    { value: "Times New Roman" },
-    { value: "Times" },
-    { value: "Courier New" },
-    { value: "Courier" },
-    { value: "Verdana" },
-    { value: "Georgia" },
-    { value: "Comic Sans MS" }
+    { value: 'monospace', selected: true },
+    { value: 'Arial' },
+    { value: 'Arial Narrow' },
+    { value: 'Helvetica' },
+    { value: 'Times New Roman' },
+    { value: 'Times' },
+    { value: 'Courier New' },
+    { value: 'Courier' },
+    { value: 'Verdana' },
+    { value: 'Georgia' },
+    { value: 'Comic Sans MS' }
   ];
 
   alignOptions = [
-    { value: "center", selected: true },
-    { value: "left" },
-    { value: "right" }
+    { value: 'center', selected: true },
+    { value: 'left' },
+    { value: 'right' }
   ];
 
-  positions = [{ value: "bottom", selected: true }, { value: "top" }];
+  positions = [{ value: 'bottom', selected: true }, { value: 'top' }];
 
-  @computed("format")
+  // eslint-disable-next-line ember/require-computed-property-dependencies
+  @computed('format')
   get currentValue() {
     let self = this;
 
-    return this.get("formats").find(function(item) {
-      if (item.value === self.get("format")) {
+    return this.formats.find(function (item) {
+      if (item.value === self.get('format')) {
         return item.code;
       }
     }).code;
   }
 
   @computed(
-    "format",
-    "width",
-    "height",
-    "displayValue",
-    "fontSize",
-    "fontOption",
-    "font",
-    "textAlign",
-    "textPosition",
-    "background",
-    "lineColor"
+    'format',
+    'width',
+    'height',
+    'displayValue',
+    'fontSize',
+    'fontOption',
+    'font',
+    'textAlign',
+    'textPosition',
+    'background',
+    'lineColor'
   )
   get demoOptions() {
     return {
-      format:       this.format || "CODE128",
+      format:       this.format || 'CODE128',
       width:        this.width || 2,
       height:       this.height || 100,
       displayValue: this.displayValue || true,
       fontSize:     this.fontSize || 20,
-      fontOptions:  this.fontOption || "",
-      font:         this.font || "monospace",
-      textAlign:    this.textAlign || "center",
-      textPosition: this.textPosition || "bottom",
-      background:   this.background || "#ffffff",
-      lineColor:    this.lineColor || "#000000"
-    }
+      fontOptions:  this.fontOption || '',
+      font:         this.font || 'monospace',
+      textAlign:    this.textAlign || 'center',
+      textPosition: this.textPosition || 'bottom',
+      background:   this.background || '#ffffff',
+      lineColor:     this.lineColor || '#000000'
+    };
   }
 
   @action
   checkValid(status) {
-    this.set("validCode", status);
+    this.set('validCode', status);
   }
 }
