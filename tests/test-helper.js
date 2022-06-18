2f7967cd14e5b02b48b2c976a9f9ea0ca40689ec
maxwondercorn <gmartell@pobox.com>
2022-06-18 01:37:18 -0400

    build: Bump ember v3.16.2...v3.20.2

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,4 +1,5 @@
-import Application from '../app';
+import Application from 'dummy/app';
+import config from 'dummy/config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
 
